ae0aa392dd3d311d28df32b574868d1a7fe35795
Abin <abin>
2024-06-14 15:01:53 +0530

    user model created

--- models/userModel.js
+++ models/userModel.js
@@ -15,5 +15,4 @@ const userSchema = new mongoose.Schema({
     }
 },{ timestamps: true })
 
-const User = mongoose.model("User", userSchema)
-module.exports = User
+export const User = mongoose.model("User", userSchema)
